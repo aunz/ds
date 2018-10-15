c0cf7879f07854888ca12e3f59b6027eb6d49f6b
Kien Ly <aunz10@yahoo.com>
2018-10-16 09:54:28 +1300

    define route for prediction

--- apps/aesop_fables/server.py
+++ apps/aesop_fables/server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import os
 from flask import Flask, request, render_template, jsonify
 
@@ -11,6 +10,14 @@ def homepage():
     """Displays the homepage."""
     return render_template('index.html')
 
+@app.route('/predict', methods=['post'])
+def route_predict():
+  text_seed = request.get_json(force=True)
+  print('Received:', text_seed['text_seed'])
+  if 'text_seed' not in text_seed: return ''
+  predicted_text = predict(text_seed['text_seed'])
+  print('Prediction done', predicted_text)
+  return predicted_text
 
 if __name__ == '__main__':
     app.run(debug=True)
